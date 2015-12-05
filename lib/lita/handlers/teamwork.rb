27868ea78e7075513c5a19b41b246793fceb4a25
yutakakinjyo <yutakakinjyo@gmail.com>
2015-12-05 04:09:23 +0000

    regist name

--- lib/lita/handlers/teamwork.rb
+++ lib/lita/handlers/teamwork.rb
@@ -1,10 +1,13 @@
 module Lita
   module Handlers
     class Teamwork < Handler
-      route(/list/, :list)
-      route(/regist/, :regist)
 
-      def regist
+      route(/list/, :list)
+      route(/regist\s+(.+)/, :regist)
+
+      def regist(response)
+        @repo = AccountRepo.instance
+        response.reply("register \"" + response.user.name + "\" to " + response.match_data[1])
       end
 
       def list
--- spec/lita/handlers/teamwork_spec.rb
+++ spec/lita/handlers/teamwork_spec.rb
@@ -5,7 +5,12 @@ describe Lita::Handlers::Teamwork, lita_handler: true do
   it { is_expected.to route("list") }
   it { is_expected.to route("list").to(:list) }
 
-  it { is_expected.to route('regist') }
-  it { is_expected.to route('regist').to(:regist) }
+  it { is_expected.to route('regist yutaka') }
+  it { is_expected.to route('regist yutaka').to(:regist) }
+
+  it "regist" do
+    send_message("regist yutaka")
+    expect(replies.last).to eq("register \"" + user.name + "\" to yutaka")
+  end
 
 end
